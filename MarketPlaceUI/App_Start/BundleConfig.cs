c10dc6ee84b9587fcb97d4d0add601157793679b
dekloni <dekloni@gmail.com>
2015-03-13 19:11:06 +0200

    test commit

--- MarketPlaceUI/App_Start/BundleConfig.cs
+++ MarketPlaceUI/App_Start/BundleConfig.cs
@@ -16,6 +16,8 @@ namespace MarketPlaceUI
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
